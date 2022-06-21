5f80ddb0e5c07138191918142946e778b69c6363
AngelAngulo19 <angelangulo19@gmail.com>
2022-06-21 17:34:51 -0300

    ultima

--- app.js
+++ app.js
@@ -6,22 +6,22 @@ app.use('/static',express.static(__dirname + '/public'));
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '/views/home.html' ));
+    res.sendFile(path.resolve(__dirname, '/Views/home.html' ));
 })
 
 app.get('/register', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '/views/register.html'));
+    res.sendFile(path.resolve(__dirname, '/Views/register.html'));
 
 })
 
 app.get('/login', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '/views/login.html'));
+    res.sendFile(path.resolve(__dirname, '/Views/login.html'));
 
 })
 
 let puerto = process.env.PORT || 3000;
 
 app.listen(puerto, () => {
-    console.log(path.resolve(__dirname, '/views/home.html' ));
+    console.log(path.resolve(__dirname, '/Views/home.html' ));
     console.log('Servidor corriendo en el puerto' + puerto);
 });
