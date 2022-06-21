f2d70d8e726169854a063735fb8a4b612bfbd048
AngelAngulo19 <angelangulo19@gmail.com>
2022-06-21 11:42:55 -0300

    fixxx

--- app.js
+++ app.js
@@ -6,16 +6,16 @@ app.use('/static',express.static(__dirname + '/public'));
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '/views/home.html' ));
+    res.sendFile(path.resolve(__dirname, './views/home.html' ));
 })
 
 app.get('/register', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '/views/register.html'));
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
 
 })
 
 app.get('/login', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '/views/login.html'));
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
 
 })
 
