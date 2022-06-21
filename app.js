b9867e3425cb06db5d22b9f29505385830f0a59e
AngelAngulo19 <angelangulo19@gmail.com>
2022-06-21 11:53:23 -0300

    ultimo

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
-const path = require('path');
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.use('/static',express.static(__dirname + '/public'));
+
+const publicPath = path.join(__dirname, './public');
+app.use(express.static(publicPath));
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/home.html' ));
+    res.sendFile(path.resolve(__dirname, '/views/home.html' ));
 })
 
 app.get('/register', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/register.html'));
+    res.sendFile(path.resolve(__dirname, '/views/register.html'));
 
 })
 
 app.get('/login', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/login.html'));
+    res.sendFile(path.resolve(__dirname, '/views/login.html'));
 
 })
 
